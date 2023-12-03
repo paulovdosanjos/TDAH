deead764213223688422dab8a6ec99b28e563b81
paulovdosanjos <paulo.gois@sptech.school>
2023-12-03 19:40:43 -0300

    BD

--- web-data-viz-main/site/src/database/script-tabelas.sql
+++ web-data-viz-main/site/src/database/script-tabelas.sql
@@ -36,48 +36,49 @@ insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar'
 comando para sql server - banco remoto - ambiente de produção
 */
 
-CREATE TABLE empresa (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	razao_social VARCHAR(50),
-	cnpj CHAR(14)
-);
+-- CREATE TABLE empresa (
+-- id INT PRIMARY KEY IDENTITY(1,1),
+-- razao_social VARCHAR(50),
+-- cnpj CHAR(14)
+-- );
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT FOREIGN KEY REFERENCES empresa(id)
-);
+-- CREATE TABLE usuario (
+-- id INT PRIMARY KEY IDENTITY(1,1),
+-- nome VARCHAR(50),
+-- email VARCHAR(50),
+-- senha VARCHAR(50),
+-- fk_empresa INT FOREIGN KEY REFERENCES empresa(id)
+-- );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT FOREIGN KEY REFERENCES usuario(id)
-);
+-- CREATE TABLE aviso (
+-- id INT PRIMARY KEY IDENTITY(1,1),
+-- titulo VARCHAR(100),
+-- descricao VARCHAR(150),
+-- fk_usuario INT FOREIGN KEY REFERENCES usuario(id)
+-- );
 
-create table aquario (
+-- create table aquario (
 /* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY IDENTITY(1,1),
-	descricao VARCHAR(300),
-	fk_empresa INT FOREIGN KEY REFERENCES empresa(id)
-);
+-- id INT PRIMARY KEY IDENTITY(1,1),
+-- descricao VARCHAR(300),
+-- 	fk_empresa INT FOREIGN KEY REFERENCES empresa(id)
+-- );
 
 /* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
 
-CREATE TABLE medida (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT FOREIGN KEY REFERENCES aquario(id)
-);
+-- CREATE TABLE medida (
+-- 	id INT PRIMARY KEY IDENTITY(1,1),
+-- 	dht11_umidade DECIMAL,
+-- dht11_temperatura DECIMAL,
+-- luminosidade DECIMAL,
+-- lm35_temperatura DECIMAL,
+-- chave TINYINT,
+-- momento DATETIME,
+-- fk_aquario INT FOREIGN KEY REFERENCES aquario(id)
+-- );
 
 insert into empresa (razao_social, cnpj) values ('Empresa 1', '00000000000000');
+
 
 /*
 comandos para criar usuário em banco de dados azure, sqlserver,
